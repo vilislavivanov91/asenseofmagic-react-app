cc754d08ac4dbd0528e641c8406fe7b27fc5958f
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-05-29 18:49:57 +0300

    Export ProductCart component as common

---
+++ src/components/common/ProductCart.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Card, CardHeader, CardBody, CardFooter, CardText, Button } from 'reactstrap'
+import ProductImage from './ProductImage'
+
+class ProductCart extends Component {
+  render () {
+    const availabilityElement = this.props.product.available ? (
+      <span className='align-self-center text-success text-center'>В наличност</span>
+    ) : (
+      <span className='not-available text-light small px-2 py-1'>Изчерпан</span>
+    )
+    const buttonElement = this.props.product.available ? (
+      <Button onClick={this.handleButtonAddProduct} color='success' block>Добави</Button>
+    ) : (
+      <Button color='primary' block>Поръчай</Button>
+    )
+    return (
+      <Card className='text-center similar-product-card'>
+        <ProductImage
+          className='similar-product-image'
+          name={this.props.product.name}
+          id={this.props.product.id}
+          src={this.props.product.src}
+          height={100} />
+        <CardBody>
+          <CardHeader tag='h5'>
+            {this.props.product.name}
+          </CardHeader>
+          {availabilityElement}
+          <CardText className='d-none d-sm-block'>
+            {this.props.product.price}лв.
+          </CardText>
+          <CardFooter>
+            {buttonElement}
+          </CardFooter>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+
+export default ProductCart
--- src/components/product/SimilarProduct.jsx
+++ src/components/product/SimilarProduct.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Card, CardHeader, CardBody, CardFooter, CardText, Button } from 'reactstrap'
-import ProductImage from '../common/ProductImage'
-import productsInCartData from '../../data/productsInCart'
+import store from '../../reduxStore'
+import { add, getTotalPrice } from '../../actions/cartAction'
+import ProductCart from '../common/ProductCart'
 
 class SimilarProduct extends Component {
   constructor (props) {
@@ -12,43 +12,13 @@ class SimilarProduct extends Component {
   }
 
   handleButtonAddProduct (e) {
-    e.preventDefault()
-
-    productsInCartData.addProduct(this.props.product)
+    store.dispatch(add(this.props.product))
+    store.dispatch(getTotalPrice())
   }
 
   render () {
-    const availabilityElement = this.props.product.available ? (
-      <span className='align-self-center text-success text-center'>В наличност</span>
-    ) : (
-      <span className='not-available text-light small px-2 py-1'>Изчерпан</span>
-    )
-    const buttonElement = this.props.product.available ? (
-      <Button onClick={this.handleButtonAddProduct} color='success' block>Добави</Button>
-    ) : (
-      <Button color='primary' block>Поръчай</Button>
-    )
     return (
-      <Card className='text-center similar-product-card'>
-        <ProductImage
-          className='similar-product-image'
-          name={this.props.product.name}
-          id={this.props.product.id}
-          src={this.props.product.src}
-          height={100} />
-        <CardBody>
-          <CardHeader tag='h5'>
-            {this.props.product.name}
-          </CardHeader>
-          {availabilityElement}
-          <CardText className='d-none d-sm-block'>
-            {this.props.product.price}лв.
-          </CardText>
-          <CardFooter>
-            {buttonElement}
-          </CardFooter>
-        </CardBody>
-      </Card>
+      <ProductCart product={this.props.product} />
     )
   }
 }
